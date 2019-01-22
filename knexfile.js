4a183a7f8a4b5b35a9aa34a733b8ae29f1b71f43
Addison Zhang <zhangx1@college.harvard.edu>
2019-01-22 12:50:50 -0500

    Set up files without location

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
-// const config = require('./config')
+const config = require('./config')
 
-// module.exports = {
-//   development: {
-//     client: 'pg',
-//     connection: config.databaseUrl,
-//     migrations: {
-//       directory: './db/migrations',
-//     },
-//     seeds: {
-//       directory: './db/seeds/dev',
-//     },
-//     useNullAsDefault: true,
-//   },
-// }
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: config.databaseUrl,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+}
